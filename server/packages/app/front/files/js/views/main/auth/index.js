b8b0ac4c755c97770d619fa355a500cef921e324
Craig Condon <craig@spiceapps.com>
2014-03-04 20:00:32 -0800

    remove transitions

--- server/packages/app/front/files/js/views/main/auth/index.js
+++ server/packages/app/front/files/js/views/main/auth/index.js
@@ -34,25 +34,7 @@ module.exports = mojo.View.extend({
         { class: require("./forgot")        , name: "forgot"        },
         { class: require("./signup")        , name: "signup"        },
         { class: require("./requestInvite") , name: "requestInvite" }
-      ],
-      transition: function (from, to, next) {
-
-        if (!process.browser) return next();
-
-        felms = (from ? from.$() : $("<div>")).filter(function (index, elm) {
-          return elm.nodeType === 1;
-        })
-        telms = to.$().filter(function (index, elm) {
-          return elm.nodeType === 1;
-        })
-        telms.css({ "display": "none", "opacity": 0 });
-
-        felms.transition({ "opacity": 0, duration: 100 }, function () {
-          felms.css({ "display": "none" });
-          telms.css({ "opacity": 0, "display": "block" });
-          telms.transition({"opacity": 1, duration: 100 }, next);
-        })
-      }
+      ]
     }
   }
 });

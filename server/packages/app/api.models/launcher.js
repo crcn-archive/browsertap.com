634ae0cf3b187ca67088377124622b0c07a56175
Craig Condon <craig@spiceapps.com>
2014-03-23 11:56:58 -0700

    call /launch on vm

--- server/packages/app/api.models/launcher.js
+++ server/packages/app/api.models/launcher.js
@@ -1,6 +1,8 @@
 var BaseModel    = require("./base/model"),
-async            = require("async");
-
+async            = require("async"),
+outcome          = require("outcome"),
+request          = require("request"),
+hurryup          = require("hurryup");
 
 function Launcher () {
   BaseModel.apply(this, arguments);
@@ -19,16 +21,41 @@ BaseModel.extend(Launcher, {
 
   launch: function (complete) {
 
-    if (!complete) complete = function () {}
+    if (!complete) complete = function () {};
+
+    var self = this;
 
     var allocator = this.app.provisioner.allocateInstance({
       userId: "abcd",
       appName: this.get("appName"),
       appVersion: this.get("appVersion"),
       maxAge: -1
-    }, complete);
+    }, outcome.e(complete).s(function (desktop) {
+      self._onDesktop(desktop);
+      complete(null, desktop);
+    }));
 
     this.set("allocator", allocator);
+  },
+
+  /**
+   */
+
+  _onDesktop: function (desktop) {
+
+    hurryup(function (self, next) {
+
+      logger.info("ping %s", desktop.addresses.publicIp);
+
+      request.post("http://" + desktop.addresses.publicIp + "/launch", {
+        form: {
+          appName: self.get("appName"),
+          appVersion: self.get("appVersion"),
+          maxAge: -1,
+          address: desktop.addresses.publicIp
+        }
+      })
+    })(this, function(){});
   }
 });
 
